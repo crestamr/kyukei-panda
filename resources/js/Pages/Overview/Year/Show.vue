3c96f8822031bb4aa75b5bcc0479ef73dc7d07c3
WINBIGFOX <winbigfox@t-online.de>
2025-05-17 20:12:45 +0200

    add event listeners for TimerStarted and TimerStopped to reload pages dynamically

--- resources/js/Pages/Overview/Year/Show.vue
+++ resources/js/Pages/Overview/Year/Show.vue
@@ -219,6 +219,18 @@ const data = {
         }
     } as ApexOptions
 }
+
+const reload = () => {
+    router.flushAll()
+    router.reload({
+        showProgress: false
+    })
+}
+
+if (window.Native) {
+    window.Native.on('App\\Events\\TimerStarted', reload)
+    window.Native.on('App\\Events\\TimerStopped', reload)
+}
 </script>
 
 <template>

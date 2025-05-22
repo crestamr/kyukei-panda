00f143e6974a6ed9738e3f2d443fa60ab32209c3
WINBIGFOX <winbigfox@t-online.de>
2025-05-22 20:31:32 +0200

    add database optimization settings and update configurations

--- config/database.php
+++ config/database.php
@@ -39,9 +39,13 @@ return [
             'database' => env('DB_DATABASE', database_path('database.sqlite')),
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
-            'busy_timeout' => 5000,
+            'busy_timeout' => 30000,
             'journal_mode' => 'WAL',
             'synchronous' => 'NORMAL',
+            'options' => [
+                PDO::ATTR_TIMEOUT => 30,
+                PDO::ATTR_EMULATE_PREPARES => false,
+            ],
         ],
 
         'mysql' => [
